a84982c3920deb5c0ac62088d0481d92e96f0a76
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2024-07-06 11:27:32 +0000

    👌👌

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
